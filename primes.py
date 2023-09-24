8319c9d858fba91ebfbdeb31f55b016ed87d4ae9
Ian Volkov <ian.volkov@kcl.ac.uk>
2023-09-24 19:16:37 +0100

    Solution 1

--- primes.py
+++ primes.py
@@ -1,6 +1,17 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isItPrime(y):
+    for i in range(2, 7):
+        if y % i = 0:
+            return True
+        else:
+            return False
+
 def primes(number_of_primes):
     list = []
+    while(len(primes) < number_of_primes):
+        y += 1
+        if isItPrime(y):
+            primes.append(y)
     return list
