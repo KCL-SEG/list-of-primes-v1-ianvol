650013fed7100e6049e661bedbb6c31383791838
Ian Volkov <ian.volkov@kcl.ac.uk>
2023-09-24 19:18:09 +0100

    Solution 2

--- primes.py
+++ primes.py
@@ -3,13 +3,14 @@
 
 def isItPrime(y):
     for i in range(2, 7):
-        if y % i = 0:
+        if y % i == 0:
             return True
         else:
             return False
 
 def primes(number_of_primes):
     list = []
+    y = 0
     while(len(primes) < number_of_primes):
         y += 1
         if isItPrime(y):
